1b7ded44241c72c6ed445bd34bdf753eee206cd5
Grayden Smith <grayden@tech-vision.ca>
2014-10-18 18:23:00 -0400

    [#80952348] Template routes for the api

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,15 @@ Kiwi::Application.routes.draw do
   get '/termsofservice', :to => 'static#termsofservice', :as => 'termsofservice'
   get '/privacy', :to => 'static#privacy', :as => 'privacy'
 
+  namespace :api do
+    api version: 1, module: 'v1' do
+      resources :events do
+        resources :comments, only: [:index, :create, :delete]
+        resources :reminders, only: [:index, :create, :delete]
+      end
+    end
+  end
+
   get '/change_password',        :to => 'passwords#change_password',  :as => 'change_password'
   get '/api/events/startupEvents',   :to => 'events#startup_events', :as => 'startup_events'
   get '/api/events/eventsAfterDate', :to => 'events#events_after_date', :as => 'events_after_date'
