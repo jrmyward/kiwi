63d495ed3ed2bec3a89d843e0ad80ef6acf8e695
Grayden Smith <grayden@tech-vision.ca>
2014-10-19 15:53:08 -0400

    [#80971264] Refactor devise views to shared layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  layout :layout_by_resource
+  layout 'application'
 
   protected
-
-  def layout_by_resource
-    if devise_controller?
-      "no_backbone"
-    else
-      "application"
-    end
-  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_in) do |u|
