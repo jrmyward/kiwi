81045d7353f5915ec293d9cfb374198e7e10daa1
Grayden Smith <grayden@tech-vision.ca>
2014-12-20 22:48:59 +0200

    [#83936116] Clean out methods and authenticate

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -59,101 +59,15 @@ class EventsController < ApplicationController
   end
 
   def show
-    # Include reminders
-    # Include event comments
-    # Include event image
     @event = Event.find(params[:id])
   end
 
   def new
+    authenticate_user!
   end
 
   def edit
-  end
-
-  def create
-    @event = Event.new()
-    params = event_params.dup
-    have_i_upvoted = params.delete :have_i_upvoted
-
-    #Picture cropping parameters need to be ready before the image is added to the model
-    #because the paperclip processor will try to use them
-    @event.width = event_params[:width]
-    @event.height = event_params[:height]
-    @event.crop_x = event_params[:crop_x]
-    @event.crop_y = event_params[:crop_y]
-
-    @event.update_attributes(params)
-
-    if (! event_params[:image] && event_params[:url])
-      @event.image_from_url(event_params[:url])
-    end
-
-    if ( user_signed_in? )
-      if ( have_i_upvoted == "true" )
-        @event.add_upvote(current_user.username)
-      else
-        @event.remove_upvote(current_user.username)
-      end
-    end
-
-    if @event.is_all_day == "true" or @event.is_all_day == true
-      @event.is_all_day = true
-    else
-      @event.is_all_day = false
-    end
-
-    respond_to do |format|
-      if @event.save
-        format.json { render action: 'show', status: :created, location: @event }
-      else
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    params = event_params.dup
-    have_i_upvoted = params.delete :have_i_upvoted
-
-    @event.width = event_params[:width]
-    @event.height = event_params[:height]
-    @event.crop_x = event_params[:crop_x]
-    @event.crop_y = event_params[:crop_y]
-
-    #this whole block of code should be in the model?
-    if (! event_params[:image] && event_params[:url])
-      @event.update_image_from_url(event_params[:url])
-    elsif !@event.image.nil?
-      @event.image.reprocess!
-    end
-
-    if ( user_signed_in? )
-      if ( have_i_upvoted == "true" )
-        @event.add_upvote(current_user.username)
-      else
-        @event.remove_upvote(current_user.username)
-      end
-    end
-
-    if @event.user == current_user.username
-      @event.update_attributes(params)
-    end
-
-    if @event.is_all_day == "true" or @event.is_all_day == true
-      @event.is_all_day = true
-    else
-      @event.is_all_day = false
-    end
-
-
-    respond_to do |format|
-      if @event.update(params)
-        format.json { render action: 'show', status: :ok, location: @event }
-      else
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
-    end
+    authenticate_user!
   end
 
   def destroy
@@ -161,9 +75,6 @@ class EventsController < ApplicationController
     respond_to do |format|
       format.json { head :no_content }
     end
-  end
-
-  def startup_events
   end
 
   def comments
