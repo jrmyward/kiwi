48237f99bb107db1c0c40ea8a518690b806fc6b3
Kevin Pratt <kevin@ouelong.com>
2015-01-12 15:46:36 -0700

    Adding form_page_base.js to asset precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Kiwi::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( weekly.js weekly.css )
+  config.assets.precompile += %w( form_page_base.js weekly.js weekly.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
