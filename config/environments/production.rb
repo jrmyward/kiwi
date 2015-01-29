bc3eab53cda37f4bd2e9b2464ab2b3f3d44a3711
Grayden Smith <grayden@tech-vision.ca>
2015-01-29 17:34:32 -0500

    [#87072876] Add to precompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Kiwi::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w(home_page_base.js show_page_base.js form_page_base.js weekly.js weekly.css )
+  config.assets.precompile += %w(home_page_base.js show_page_base.js form_page_base.js weekly.js weekly.css time_zone_set.js)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
